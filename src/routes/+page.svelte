ccc21f7e33c8989119787c5b89d197b19debeca9
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2023-05-16 18:46:12 +0900

    Fix: Remove browser-specific styling in input fields This allows input borders to be displayed correctly in Safari.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -110,7 +110,7 @@
               name="subject"
               type="text"
               required
-              class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+              class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 appearance-none"
             />
           </div>
         </div>
@@ -129,7 +129,7 @@
               type="email"
               autocomplete="email"
               required
-              class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+              class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 appearance-none"
             />
           </div>
         </div>
